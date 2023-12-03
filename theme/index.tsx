2e66254d74c02825b74e26724160d7cbd7eb2022
sumy <sumyggsun@gmail.com>
2023-12-04 00:48:11 +0800

    fix: only layout:post show postInfo.

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import Theme from '@rspress/theme-default'
-import { Helmet } from 'rspress/runtime'
+import { Helmet, usePageData } from 'rspress/runtime'
 import PostFooter from './components/PostFooter'
 import PostInfo from './components/PostInfo'
 import Footer from './components/Footer'
 
 import './index.scss'
 
-const Layout = () => (
-  <>
-    {/* katex css */}
-    <Helmet>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
-        integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV"
-        crossOrigin="anonymous"
+const Layout = () => {
+  const pageData = usePageData()
+  return (
+    <>
+      {/* katex css */}
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
+          integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV"
+          crossOrigin="anonymous"
+        />
+      </Helmet>
+      <Theme.Layout
+        beforeDocFooter={<PostFooter />}
+        beforeOutline={
+          pageData.page.frontmatter.layout === 'post' && <PostInfo />
+        }
+        bottom={<Footer />}
       />
-    </Helmet>
-    <Theme.Layout
-      beforeDocFooter={<PostFooter />}
-      beforeOutline={<PostInfo />}
-      bottom={<Footer />}
-    />
-  </>
-)
-
+    </>
+  )
+}
 export default {
   ...Theme,
   Layout,
