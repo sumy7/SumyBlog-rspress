2358de7bdca2459e5992cde72aa3cd658f8495df
sumy <sumyggsun@gmail.com>
2024-01-04 03:04:27 +0800

    chore: 去掉google ad组件key。

--- theme/components/PostFooter/index.tsx
+++ theme/components/PostFooter/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useLocation } from 'rspress/runtime'
 import GiscusComments from '../../GiscusComments'
 import PrevNextPage from '../PrevNextPage'
 import GoogleAds from '../../GoogleAds'
@@ -7,15 +6,10 @@ import GoogleAds from '../../GoogleAds'
 import styles from './index.module.scss'
 
 const PostFooter = () => {
-  const location = useLocation()
-
   return (
     <div className={`${styles.postFooter} flex flex-col`}>
       <PrevNextPage />
-      <GoogleAds
-        key={'google-ads-' + location.pathname}
-        dataAdSlot={'7247705093'}
-      />
+      <GoogleAds dataAdSlot={'7247705093'} />
       <GiscusComments />
     </div>
   )
