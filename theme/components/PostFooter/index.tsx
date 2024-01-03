3a33e0c4a51b8b562ab5d9a65e7f78af4773f24d
sumy <sumyggsun@gmail.com>
2024-01-04 00:59:25 +0800

    feat: refresh ads when router change.

--- theme/components/PostFooter/index.tsx
+++ theme/components/PostFooter/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useLocation } from 'rspress/runtime'
 import GiscusComments from '../../GiscusComments'
 import PrevNextPage from '../PrevNextPage'
 import GoogleAds from '../../GoogleAds'
@@ -6,10 +7,15 @@ import GoogleAds from '../../GoogleAds'
 import styles from './index.module.scss'
 
 const PostFooter = () => {
+  const location = useLocation()
+
   return (
     <div className={`${styles.postFooter} flex flex-col`}>
       <PrevNextPage />
-      <GoogleAds dataAdSlot={'7247705093'} />
+      <GoogleAds
+        key={'google-ads-' + location.pathname}
+        dataAdSlot={'7247705093'}
+      />
       <GiscusComments />
     </div>
   )
