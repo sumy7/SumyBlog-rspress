9a75e072456180380410c65e9694cebd54665065
sumy <sumyggsun@gmail.com>
2024-03-14 20:04:50 +0800

    chore: config social link, add rss link.

--- blog/rspress.config.ts
+++ blog/rspress.config.ts
@@ -48,7 +48,14 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', mode: 'link', content: 'https://github.com/sumy7/' },
       { icon: 'weibo', mode: 'link', content: 'https://weibo.com/sumy7' },
-      { icon: 'twitter', mode: 'link', content: 'https://twitter.com/sumygg' },
+      { icon: 'x', mode: 'link', content: 'https://twitter.com/sumygg' },
+      {
+        icon: {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="currentColor" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm1.5 2.5c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0a8 8 0 0 0-8-8a1 1 0 0 1 0-2m0 4a6 6 0 0 1 6 6a1 1 0 1 1-2 0a4 4 0 0 0-4-4a1 1 0 0 1 0-2m.5 7a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3"/></svg>',
+        },
+        mode: 'link',
+        content: 'https://sumygg.com/rss/feed.rss',
+      },
     ],
     giscus: {
       repo: 'sumy7/sumy7.github.io',
